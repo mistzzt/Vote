37c2b659c7616d842017e415f3dfe6ec700ada2a
Zt Wang <mymisting@gmail.com>
2016-06-10 15:25:48 +0800

    Initial debut of plugin Vote

---
+++ Vote/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Vote")]
+[assembly: AssemblyDescription("Gives players the right to vote in Terraria server running TShock.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MistZZT")]
+[assembly: AssemblyProduct("Vote")]
+[assembly: AssemblyCopyright("Copyright © MistZZT 2016")]
+[assembly: AssemblyTrademark("MistZZT")]
+[assembly: AssemblyCulture("en-US")]
+
+[assembly: ComVisible(false)]
+
+// 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
+[assembly: Guid("8016e0f2-824b-4a07-9488-f4a49765854b")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ Vote/VotePlugin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaApi.Server;
+using Terraria;
+using TShockAPI;
+using TShockAPI.DB;
+
+namespace Vote {
+	public class VotePlugin:TerrariaPlugin {
+		public const string VotePlayerData = "votedata";
+
+		public override string Name => "Vote";
+		public override string Author => "MistZZT";
+		public override string Description => "Gives players the right to vote in Terraria server running TShock.";
+		public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+		public VotePlugin(Main game) : base(game) { }
+
+		public override void Initialize() {
+			ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
+			ServerApi.Hooks.ServerLeave.Register(this, OnLeave);
+		}
+
+		protected override void Dispose(bool disposing) {
+			if(disposing) {
+				ServerApi.Hooks.ServerJoin.Deregister(this, OnJoin);
+				ServerApi.Hooks.ServerLeave.Deregister(this, OnLeave);
+			}
+			base.Dispose(disposing);
+		}
+
+		private void OnJoin(JoinEventArgs args)
+			=> TShock.Players[args.Who]?.SetData(VotePlayerData, new PlayerData());
+
+		private void OnLeave(LeaveEventArgs args)
+			=> TShock.Players[args.Who]?.RemoveData(VotePlayerData);
+	}
+}
